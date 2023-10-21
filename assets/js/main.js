43e9d7098c8f4f96379e7c7b2af14940c10e1051
Robert Anisimov <skillup23@yandex.ru>
2023-10-21 22:37:23 +0300

    Версия 0.8

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
-  const header = document.getElementById('header');
+  const header = document.getElementById("header");
   // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 50) header.classList.add('scroll-header');
-  else header.classList.remove('scroll-header');
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader);
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER POPULAR ===============*/
 // var swiperPopular = new Swiper(".popular__container", {
@@ -22,13 +22,13 @@ window.addEventListener('scroll', scrollHeader);
 // });
 
 /*=============== VALUE ACCORDION ===============*/
-const accordionItem = document.querySelectorAll('.value__accordion-item');
+const accordionItem = document.querySelectorAll(".value__accordion-item");
 
 accordionItem.forEach((item) => {
-  const accordionHeader = item.querySelector('.value__accordion-header');
+  const accordionHeader = item.querySelector(".value__accordion-header");
 
-  accordionHeader.addEventListener('click', () => {
-    const openItem = document.querySelector('.accordion-open');
+  accordionHeader.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-open");
 
     toggleItem(item);
 
@@ -39,155 +39,155 @@ accordionItem.forEach((item) => {
 });
 
 const toggleItem = (item) => {
-  const accordionContent = item.querySelector('.value__accordion-content');
+  const accordionContent = item.querySelector(".value__accordion-content");
 
-  if (item.classList.contains('accordion-open')) {
-    accordionContent.removeAttribute('style');
-    item.classList.remove('accordion-open');
+  if (item.classList.contains("accordion-open")) {
+    accordionContent.removeAttribute("style");
+    item.classList.remove("accordion-open");
   } else {
-    accordionContent.style.height = accordionContent.scrollHeight + 'px';
-    item.classList.add('accordion-open');
+    accordionContent.style.height = accordionContent.scrollHeight + "px";
+    item.classList.add("accordion-open");
   }
 };
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]');
+const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
   const scrollY = window.pageYOffset;
 
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight,
-      sectionTop = current.offsetTop - 58,
-      sectionId = current.getAttribute('id');
+      sectionTop = current.offsetTop - 200,
+      sectionId = current.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
-        .querySelector('.nav__menu a[href*=' + sectionId + ']')
-        .classList.add('active-link');
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
     } else {
       document
-        .querySelector('.nav__menu a[href*=' + sectionId + ']')
-        .classList.remove('active-link');
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
     }
   });
 }
-window.addEventListener('scroll', scrollActive);
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
 function scrollUp() {
-  const scrollUp = document.getElementById('scroll-up');
+  const scrollUp = document.getElementById("scroll-up");
   // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
-  else scrollUp.classList.remove('show-scroll');
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener('scroll', scrollUp);
+window.addEventListener("scroll", scrollUp);
 
 /*=============== DARK LIGHT THEME ===============*/
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
-const iconTheme = 'bx-sun';
+// const themeButton = document.getElementById("theme-button");
+// const darkTheme = "dark-theme";
+// const iconTheme = "bx-sun";
 
-// Previously selected topic (if user selected)
-// Ранее выбранная тема (если выбрана пользователем)
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
+// // Previously selected topic (if user selected)
+// // Ранее выбранная тема (если выбрана пользователем)
+// const selectedTheme = localStorage.getItem("selected-theme");
+// const selectedIcon = localStorage.getItem("selected-icon");
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-// Мы получаем текущую тему интерфейса, проверяя класс темной темы
-const getCurrentTheme = () =>
-  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
-const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun';
+// // We obtain the current theme that the interface has by validating the dark-theme class
+// // Мы получаем текущую тему интерфейса, проверяя класс темной темы
+// const getCurrentTheme = () =>
+//   document.body.classList.contains(darkTheme) ? "dark" : "light";
+// const getCurrentIcon = () =>
+//   themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
 
-// We validate if the user previously chose a topic
-// Мы проверяем, выбрал ли пользователь ранее тему
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  // Если проверка выполнена, мы спрашиваем, в чем проблема, чтобы узнать, активировали мы или деактивировали темную
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
-    darkTheme
-  );
-  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](
-    iconTheme
-  );
-}
+// // We validate if the user previously chose a topic
+// // Мы проверяем, выбрал ли пользователь ранее тему
+// if (selectedTheme) {
+//   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+//   // Если проверка выполнена, мы спрашиваем, в чем проблема, чтобы узнать, активировали мы или деактивировали темную
+//   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+//     darkTheme
+//   );
+//   themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](
+//     iconTheme
+//   );
+// }
 
-// Activate / deactivate the theme manually with the button
-// Активировать/деактивировать тему вручную кнопкой
-themeButton.addEventListener('click', () => {
-  // Add or remove the dark / icon theme
-  // Добавить или удалить темную тему / тему значков
-  document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
-  // Сохраняем тему и текущую иконку, которую выбрал пользователь
-  localStorage.setItem('selected-theme', getCurrentTheme());
-  localStorage.setItem('selected-icon', getCurrentIcon());
-});
+// // Activate / deactivate the theme manually with the button
+// // Активировать/деактивировать тему вручную кнопкой
+// themeButton.addEventListener("click", () => {
+//   // Add or remove the dark / icon theme
+//   // Добавить или удалить темную тему / тему значков
+//   document.body.classList.toggle(darkTheme);
+//   themeButton.classList.toggle(iconTheme);
+//   // We save the theme and the current icon that the user chose
+//   // Сохраняем тему и текущую иконку, которую выбрал пользователь
+//   localStorage.setItem("selected-theme", getCurrentTheme());
+//   localStorage.setItem("selected-icon", getCurrentIcon());
+// });
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-  origin: 'top',
-  distance: '60px',
+  origin: "top",
+  distance: "60px",
   duration: 2500,
   delay: 400,
   reset: false,
 });
 
 sr.reveal(
-  `.home__title,.home__subtitle, .popular__container, .subscribe__container, .footer__container`
+  `.home__title-box, .home__subtitle, .subscribe__container, .footer__container`
 );
-sr.reveal(`.home__description, .footer__info`, { delay: 500 });
+sr.reveal(`.section__title, .footer__info`, { delay: 500 });
 sr.reveal(`.home__search`, { delay: 600 });
-sr.reveal(`.home__value`, { delay: 700 });
-sr.reveal(`.home__images`, { delay: 800, origin: 'bottom' });
+sr.reveal(`.home__value, .info, .services`, { delay: 700 });
+sr.reveal(`.home__video`, { delay: 800, origin: "bottom" });
 sr.reveal(`.logos__img`, { interval: 100 });
-sr.reveal(`.value__images, .contact__content`, { origin: 'left' });
-sr.reveal(`.value__content, .contact__images`, { origin: 'right' });
+sr.reveal(`.value__images, .contact__content`, { origin: "left" });
+sr.reveal(`.value__content, .contact__images`, { origin: "right" });
 
 /*=============== Telephone mask ===============*/
-window.addEventListener('DOMContentLoaded', function () {
-  [].forEach.call(
-    document.querySelectorAll('.home__search-input'),
-    function (input) {
-      var keyCode;
-      function mask(event) {
-        event.keyCode && (keyCode = event.keyCode);
-        var pos = this.selectionStart;
-        if (pos < 3) event.preventDefault();
-        var matrix = '+7 (___) ___ ____',
-          i = 0,
-          def = matrix.replace(/\D/g, ''),
-          val = this.value.replace(/\D/g, ''),
-          new_value = matrix.replace(/[_\d]/g, function (a) {
-            return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
-          });
-        i = new_value.indexOf('_');
-        if (i != -1) {
-          i < 5 && (i = 3);
-          new_value = new_value.slice(0, i);
-        }
-        var reg = matrix
-          .substr(0, this.value.length)
-          .replace(/_+/g, function (a) {
-            return '\\d{1,' + a.length + '}';
-          })
-          .replace(/[+()]/g, '\\$&');
-        reg = new RegExp('^' + reg + '$');
-        if (
-          !reg.test(this.value) ||
-          this.value.length < 5 ||
-          (keyCode > 47 && keyCode < 58)
-        )
-          this.value = new_value;
-        if (event.type == 'blur' && this.value.length < 5) this.value = '';
-      }
+// window.addEventListener("DOMContentLoaded", function () {
+//   [].forEach.call(
+//     document.querySelectorAll(".home__search-input"),
+//     function (input) {
+//       var keyCode;
+//       function mask(event) {
+//         event.keyCode && (keyCode = event.keyCode);
+//         var pos = this.selectionStart;
+//         if (pos < 3) event.preventDefault();
+//         var matrix = "+7 (___) ___ ____",
+//           i = 0,
+//           def = matrix.replace(/\D/g, ""),
+//           val = this.value.replace(/\D/g, ""),
+//           new_value = matrix.replace(/[_\d]/g, function (a) {
+//             return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+//           });
+//         i = new_value.indexOf("_");
+//         if (i != -1) {
+//           i < 5 && (i = 3);
+//           new_value = new_value.slice(0, i);
+//         }
+//         var reg = matrix
+//           .substr(0, this.value.length)
+//           .replace(/_+/g, function (a) {
+//             return "\\d{1," + a.length + "}";
+//           })
+//           .replace(/[+()]/g, "\\$&");
+//         reg = new RegExp("^" + reg + "$");
+//         if (
+//           !reg.test(this.value) ||
+//           this.value.length < 5 ||
+//           (keyCode > 47 && keyCode < 58)
+//         )
+//           this.value = new_value;
+//         if (event.type == "blur" && this.value.length < 5) this.value = "";
+//       }
 
-      input.addEventListener('input', mask, false);
-      input.addEventListener('focus', mask, false);
-      input.addEventListener('blur', mask, false);
-      input.addEventListener('keydown', mask, false);
-    }
-  );
-});
+//       input.addEventListener("input", mask, false);
+//       input.addEventListener("focus", mask, false);
+//       input.addEventListener("blur", mask, false);
+//       input.addEventListener("keydown", mask, false);
+//     }
+//   );
+// });
